a1f829bfa681f3c9add8e3f3f3327adec2782134
L3dlightening <ch4nge.lightening@gmail.com>
2020-09-07 20:58:58 +0900

    send message when you enter or leave voice channel

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,5 @@
 import configparser
+import discord
 from discord.ext import commands
 
 # configファイルの読み込み
@@ -7,8 +8,8 @@ config.read('./config.ini', 'UTF-8')
 
 # discordに必要な情報
 TOKEN = config.get('discord', 'TOKEN')
-text_id = config.get('channel', 'text_test')
-voice_id = config.get('channel', 'voice_test')
+# text_id = config.get('channel', 'text_test')
+# voice_id = config.get('channel', 'voice_test')
 
 bot = commands.Bot(command_prefix='$')
 
@@ -20,15 +21,16 @@ async def on_ready():
 async def neko(ctx):
     await ctx.send('にゃーん')
 
-# move!!!!
-'''
 @bot.event
-async def on_voice_state_update(before, after):
-    if before.voice_channel is None:
-        alert_channel = bot.get_channel(text_id)
-        name = after.name
-        msg = f'{name}が参加しました。'
+async def on_voice_state_update(member, before, after):
+    alert_channel = discord.utils.get(member.guild.text_channels, name='botテキスト')
+
+    if before.channel is None:
+        msg = f'{member.name}が参加しました'
         await alert_channel.send(msg)
-'''
+    elif after.channel is None:
+        msg = f'{member.name}が退出しました'
+        await alert_channel.send(msg)
+        
 
 bot.run(TOKEN)
